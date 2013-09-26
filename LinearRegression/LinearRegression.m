7230c5a58f58b6a825e8de4b4619bdfd1242be2e
Phillip Porch <ppporch@theporch.com>
2013-09-26 15:11:11 -0500

    Initial Commit

---
+++ LinearRegression/LinearRegression.m
@@ -0,0 +1,13 @@
+//
+//  LinearRegression.m
+//  LinearRegression
+//
+//  Created by Phillip Porch on 9/26/13.
+//  Copyright (c) 2013 Phillip Porch. All rights reserved.
+//
+
+#import "LinearRegression.h"
+
+@implementation LinearRegression
+
+@end
---
+++ LinearRegressionTests/LinearRegressionTests.m
@@ -0,0 +1,34 @@
+//
+//  LinearRegressionTests.m
+//  LinearRegressionTests
+//
+//  Created by Phillip Porch on 9/26/13.
+//  Copyright (c) 2013 Phillip Porch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LinearRegressionTests : XCTestCase
+
+@end
+
+@implementation LinearRegressionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
